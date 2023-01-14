8c115ca5a468496090f3a38bdcc9fcd084f27eb8
Killermaschine88 <ybaltraz@gmail.com>
2023-01-14 12:29:37 +0100

    fix(dmg render again)

--- functions/features/misc/damageRenderUtils.js
+++ functions/features/misc/damageRenderUtils.js
@@ -14,15 +14,16 @@ export const getDamageScale = (type) => {
 
 export const isValidDamageEntity = (entity) => {
     const entityName = entity.getName();
+    if(!isNaN(Number(entityName.replaceAll(",", "").replaceAll("✧", "")))) return false; // Ignoring Non Numbers
     if (entityName.includes("§8[")) return false; // Ignoring Mob Nametags
     if (entityName.includes(" ")) return false; // Ignoring Stuff with Spaces as DMG ArmorStands don't have spaces
 
     if (!isNaN(Number(entityName))) return true; // Non Crits
     if (entityName.includes("✧")) return true; // Crits
     if (entityName.includes("§2")) return true; // Venomous Damage
-    if (entityName.includes("§6") && isNaN(Number(entityName.replace(",", "")))) return true; // Fire Aspect Damage
+    if (entityName.includes("§6") && isNaN(Number(entityName.replaceAll(",", "")))) return true; // Fire Aspect Damage
     if (entityName.includes("§7")) return true; // True Damage
-    if (entityName.includes("§9") && !entityName.includes("✦")) return true; // IDFK what this damage type is but it exists
+    if (entityName.includes("§3") && !(entityName.includes("✦"))) return true; // IDFK what this damage type is but it exists
 
     // Nothing passed
     return false;
