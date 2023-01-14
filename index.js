b64f7b01271b5a3d7c7a37dfdd6dc961eb76fcaa
Killermaschine88 <ybaltraz@gmail.com>
2023-01-14 15:13:11 +0100

    fix(rename files)

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 import "./features/commands/mainCommand";
 
 // Dungeons
-import "./features/dungeons/dungeonParty";
+import "./features/dungeons/dungeonPartyInfo";
 
 // Crimson Isles
 import "./features/crimsonIsles/mobHitboxes";
+import "./features/crimsonIsles/kuudraPartyInfo"
 
 // Misc
 import "./features/misc/damageRenderer";
