75d57c3c27f098417acac5a512f6423b15e6e37a
BongoBerger <slottshot@gmail.com>
2023-01-15 09:52:10 -0500

    wllo

--- features/misc/hideAbiphone.js
+++ features/misc/hideAbiphone.js
@@ -1,20 +1,26 @@
 import Settings from "../../constants/settings";
 import { prefix } from "../../constants/variables";
 
-let cancelRinging = false;
+const ignoredCallers = ["&r&e✆ &r&cAranya&r&e ✆ &r", "&r&e✆ &r&dKaus&r&e ✆ &r", "&r&e✆ &r&5Rollim&r&e ✆ &r"];
 
 register("Chat", (event) => {
+
     if (!Settings.cancelAbiphone) return;
+
+    let cancelRinging = false;
+
     const message = ChatLib.getChatMessage(event, true);
-    if (message.includes("&r&e✆ &r&cAranya&r&e ✆ &r") || message.includes("&r&e✆ &r&dKaus&r&e ✆ &r") || message.includes("&r&e✆ &r&5Rollim&r&e ✆ &r")) {
-        let ignoredCaller
-        if (message.includes("&r&e✆ &r&cAranya&r&e ✆ &r")); ignoredCaller = "&cAranya";
-        if (message.includes("&r&e✆ &r&dKaus&r&e ✆ &r")); ignoredCaller = "&dKaus";
-        if (message.includes("&r&e✆ &r&5Rollim&r&e ✆ &r")); ignoredCaller = "&5Rollim";
-        ChatLib.chat(`${prefix} ${ignoredCaller} &7attempted to call you.`)
+
+    if (message.includes(ignoredCallers)) {
+        let theCaller
+        if (message.includes(ignoredCallers[0])); theCaller = "&cAranya";
+        if (message.includes(ignoredCallers[1])); theCaller = "&dKaus";
+        if (message.includes(ignoredCallers[2])); theCaller = "&5Rollim";
+        ChatLib.chat(`${prefix} ${theCaller} &7attempted to call you.`)
         cancelRinging = true;
         cancel(event)
     }
+
     if (cancelRinging) {
         if (message.includes("&r&a✆ Ring...") || message.includes("&r&a✆ Ring... Ring...")) {
             cancel(event)
