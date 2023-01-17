33cdb06e6b87e9baee0da5be86a56267ff9219d3
baltraz <you@example.com>
2023-01-17 09:53:50 +0000

    feat(fix shit comments and document mcp functions)

---
+++ features/customSkyblockMenu/createMenu.js
@@ -0,0 +1,101 @@
+/// <reference types="../../CTAutocomplete" />
+/// <reference lib="es2015" />
+
+import Settings from "../../constants/settings";
+import { skullInSlot, itemInSlot } from "../../functions/features/customSkyblockMenu/nbt";
+
+const GuiChest = Java.type("net.minecraft.client.gui.inventory.GuiChest");
+const InventoryBasic = Java.type("net.minecraft.inventory.InventoryBasic");
+const Mc = Client.getMinecraft();
+
+let firstTimeMenu = true; // optimization!!!
+let generatedMenu = false; // optimization!!!
+let firstTimeItem = true; // optimization!!!
+let generatedItem = false; // optimization!!!
+
+// custom menu GUI
+let menuInv = new InventoryBasic("§cCustom SkyBlock Menu§r", true, 54); //creates a basic inventory with custom name and 54 slots (cakend gave this code w let so idk)
+let menuGui = new GuiChest(Player.getPlayer().field_71071_by, menuInv); //makes a chest out of the players inv and then new inventory (cakend gave this code w let so idk)
+
+// item selector GUI
+let itemInv = new InventoryBasic("§cButton Picker§r", true, 54); //creates a basic inventory with custom name and 54 slots (cakend gave this code w let so idk)
+let itemGui = new GuiChest(Player.getPlayer().field_71071_by, itemInv); //makes a chest out of the players inv and then new inventory (cakend gave this code w let so idk)
+
+register("postGuiRender", (gui) => {
+    if (!Settings.skyblockMenu) return;
+
+    /*                          Custom Skyblock Menu                        */
+    if (Player.getContainer()?.toString()?.includes("Custom SkyBlock Menu")) {
+        if (firstTimeMenu && !generatedMenu) {
+            firstTime = false;
+            generated = true;
+            // this is just here as an example for how uuid and texture work
+            let uuid = "e780adce-f739-4c15-9e5b-a78562e2c935";
+            let texture = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGNiM2FjZGMxMWNhNzQ3YmY3MTBlNTlmNGM4ZTliM2Q5NDlmZGQzNjRjNjg2OTgzMWNhODc4ZjA3NjNkMTc4NyJ9fX0=";
+        }
+    }
+
+    //////////////////////////// ITEM SELECTOR ////////////////////////////
+    if (Player.getContainer()?.toString()?.includes("§cButton Picker§r")) {
+        console.log(JSON.stringify(Player?.getContainer()?.getStackInSlot(0)?.getLore()));
+        if (firstTimeItem && !generatedItem) {
+            firstTimeItem = false;
+            generatedItem = true;
+
+            itemInSlot(0, "test", ["§7Click to open the Skyblock Menu§r", "§cim a genius ngl§r"], "paper");
+        }
+    }
+});
+
+register("command", () => {
+    GuiHandler.openGui(menuGui); // opens the gui
+}).setName("test");
+
+// this creates a new console that logs packets to it you can access it with /packetlogger to add stuff to console do c.println("balls")
+
+const con = com.chattriggers.ctjs.utils.console.Console;
+const c = new con(null);
+
+register("Command", () => {
+    c.showConsole();
+}).setName("packetlogger");
+
+register("packetSent", (packet) => {
+    if (!packet.toString().includes("C03") && !packet.toString().includes("C0F") && !packet.toString().includes("C00")) {
+        c.println(packet.toString());
+    }
+});
+
+// this stops you from moving the items around while still telling us that you clicked that slot
+register("guiMouseClick", (mx, my, btn, gui, event) => {
+    if (Player.getContainer().getName().equals("§cCustom SkyBlock Menu§r")) {
+        if (Client.isShiftDown()) {
+            Client.currentGui.close();
+            GuiHandler.openGui(itemGui);
+        }
+        // cancel the click so no item move
+        cancel(event);
+    }
+    if (Player.getContainer().getName().equals("§cButton Picker")) return cancel(event);
+});
+
+register("itemTooltip", (lore, item, event) => {
+    if (item.getName().equals("")) {
+        cancel(event);
+    }
+});
+
+/*
+// examples of ways to do stuff
+
+Client.currentGui.getSlotUnderMouse().getIndex() // gets the slot you are hovering over
+
+
+
+
+
+
+
+
+
+*/
