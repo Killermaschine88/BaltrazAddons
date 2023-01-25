f7d80a324e8431852911bc4454bb8b474a69d902
Killermaschine88 <ybaltraz@gmail.com>
2023-01-25 17:43:16 +0100

    remove(useless pogdata object)

--- constants/variables.js
+++ constants/variables.js
@@ -1,14 +1,1 @@
-import PogObject from "../../PogData/index";
-
 export const prefix = "&8[&6BA&8]";
-
-export const data = new PogObject(
-    "BaltrazAddons",
-    {
-        partyDisplay: {
-            x: 0,
-            y: 0,
-        },
-    },
-    "constants/data.json"
-);
